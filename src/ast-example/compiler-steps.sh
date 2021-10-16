1b55254c60b931a382725a1f6206154c27b4455c
Robin Elvedi <trivialic@posteo.net>
2021-10-16 11:42:47 +0200

    modify script

--- src/ast-example/compiler-steps.sh
+++ src/ast-example/compiler-steps.sh
@@ -2,6 +2,6 @@
 # -dsuppress-all
 # -dsuppress-idinfo
 
-stack ghc -- Main.hs -dsuppress-timestamps -dsuppress-all -ddump-to-file -ddump-parsed -ddump-parsed-ast -ddump-rn -ddump-rn-ast -ddump-tc -ddump-tc-ast -ddump-ds-preopt -ddump-simpl-iterations -ddump-simpl-stats -ddump-rule-rewrites -ddump-stg -ddump-cmm-raw -ddump-asm-native 
+stack ghc -- Main.hs -dsuppress-timestamps -dsuppress-all -ddump-to-file -ddump-parsed-ast -ddump-rn-ast -ddump-tc-ast -ddump-ds-preopt -ddump-stg -ddump-cmm-raw -ddump-asm-native 
 
 rm Main.hi Main.o
