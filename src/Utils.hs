d22882b549898de297cb2f6c7aefb1336fff7fcf
Robin Elvedi <trivialic@posteo.net>
2021-10-26 08:47:34 +0200

    add standalone parser example code

---
+++ src/Utils.hs
@@ -0,0 +1,15 @@
+-- https://github.com/tweag/ormolu/blob/897e6736ec17d7e359091abe3c24f4abfec015c4/src/Ormolu/Utils.hs
+
+module Utils where
+
+import DynFlags (baseDynFlags)
+import qualified GHC.Utils.Outputable as GHC
+import Data.Data ( Data )
+import GHC.Hs.Dump (showAstData, BlankSrcSpan (BlankSrcSpan))
+
+showOutputable :: GHC.Outputable o => o -> String
+showOutputable = GHC.showSDoc baseDynFlags . GHC.ppr
+
+-- https://hackage.haskell.org/package/ghc-lib-parser-9.0.1.20210324/docs/GHC-Hs-Dump.html
+dumpAST :: Data a => a -> String
+dumpAST x = showOutputable $ showAstData BlankSrcSpan x
