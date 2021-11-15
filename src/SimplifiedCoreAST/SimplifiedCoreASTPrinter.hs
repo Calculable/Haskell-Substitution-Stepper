fc07e6bc9df3aa0a498d93b76620ad5f28670f82
Jan Huber <jan.huber@ost.ch>
2021-11-15 13:26:32 +0100

    Improved Indentation

--- src/SimplifiedCoreAST/SimplifiedCoreASTPrinter.hs
+++ src/SimplifiedCoreAST/SimplifiedCoreASTPrinter.hs
@@ -13,53 +13,60 @@ printSimplifiedCoreBinding :: BindS -> IO ()
 printSimplifiedCoreBinding (x, expression) = do
     putStr x
     putStr " = "
-    printSimplifiedCoreExpression expression
+    printSimplifiedCoreExpression expression 0
     putStrLn ""
 
-printSimplifiedCoreExpression :: ExpressionS -> IO ()
-printSimplifiedCoreExpression (VarS var) = do
+printSimplifiedCoreExpression :: ExpressionS -> Integer -> IO ()
+printSimplifiedCoreExpression (VarS var) indent  = do
     if (isPrefixOf "$" var)
       then putStr ""
       else putStr var
-printSimplifiedCoreExpression (LitS lit) = printLiteral lit
-printSimplifiedCoreExpression (AppS exp arg) = do
+printSimplifiedCoreExpression (LitS lit) indent = printLiteral lit indent
+printSimplifiedCoreExpression (AppS exp arg) indent = do
   putStr "("
-  printSimplifiedCoreExpression exp
+  printSimplifiedCoreExpression exp indent
   putStr " "
-  printSimplifiedCoreExpression arg
+  printSimplifiedCoreExpression arg indent
   putStr ")"
-printSimplifiedCoreExpression (LamS b exp) = do
+printSimplifiedCoreExpression (LamS b exp) indent= do
   putStr "\\"
   putStr b
   putStr " -> ("
-  printSimplifiedCoreExpression exp
+  printSimplifiedCoreExpression exp indent
   putStr ")"
-printSimplifiedCoreExpression (TypeS) = putStr ""
-printSimplifiedCoreExpression (CaseS exp alts) = do
+printSimplifiedCoreExpression (TypeS) indent = putStr ""
+printSimplifiedCoreExpression (CaseS exp alts) indent = do
   putStr "case "
-  printSimplifiedCoreExpression exp
+  printSimplifiedCoreExpression exp indent
   putStr " of {"
-  mapM_ printSimplifiedAlt alts
-  putStr "};"
-printSimplifiedCoreExpression (InvalidExpression reason) = do
+  mapM_ (printSimplifiedAlt (indent+1)) alts
+  putStrLn("")
+  printDepth indent
+  putStr "}"
+printSimplifiedCoreExpression (InvalidExpression reason) indent = do
   putStr ("Unsupported Expression " ++ reason)
 
-printLiteral :: LiteralS -> IO ()
-printLiteral (LitCharS c) = putStr (show c)
-printLiteral (LitNumberS v) = putStr (show v)
-printLiteral (LitStringS s) = putStr s
-printLiteral (LitFloatS f) = putStr (show f)
-printLiteral (LitDoubleS d) = putStr (show d)
+printLiteral :: LiteralS -> Integer -> IO ()
+printLiteral (LitCharS c) indent = putStr (show c)
+printLiteral (LitNumberS v) indent = putStr (show v)
+printLiteral (LitStringS s) indent = putStr s
+printLiteral (LitFloatS f) indent = putStr (show f)
+printLiteral (LitDoubleS d) indent = putStr (show d)
 
-printSimplifiedAlt :: AltS -> IO ()
-printSimplifiedAlt (altCon, b, exp) = do
-    putStr "\n\t"
-    printSimplifiedAltCon altCon --either literal or constructor or _
+printSimplifiedAlt :: Integer -> AltS -> IO ()
+printSimplifiedAlt indent (altCon, b, exp) = do
+    putStrLn("")
+    printDepth indent
+    printSimplifiedAltCon indent altCon --either literal or constructor or _
     putStr (unwords b)
     putStr " -> "
-    printSimplifiedCoreExpression exp
+    printSimplifiedCoreExpression exp indent
+    putStr ";"
 
-printSimplifiedAltCon :: AltConS -> IO ()
-printSimplifiedAltCon (DataAltS x) = putStr x
-printSimplifiedAltCon (LitAltS x) = printLiteral x
-printSimplifiedAltCon (DefaultS) = putStr "_"
+printSimplifiedAltCon :: Integer -> AltConS -> IO ()
+printSimplifiedAltCon indent (DataAltS x) = putStr x
+printSimplifiedAltCon indent (LitAltS x) = printLiteral x indent
+printSimplifiedAltCon indent (DefaultS) = putStr "_"
+
+printDepth :: Integer -> IO ()
+printDepth depth = putStr (replicate (fromInteger depth) '\t')
