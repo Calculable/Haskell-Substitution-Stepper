5314ab2ce3c288545c53ce6c25ae5d49fc78407a
Jan Huber <jan.huber@ost.ch>
2021-12-01 16:56:03 +0100

    Ad Files CoreLookup and CoreTransformator

--- src/OriginalCoreAST/CoreStepper.hs
+++ src/OriginalCoreAST/CoreStepper.hs
@@ -26,6 +26,8 @@ import GHC.Types.Name.Occurrence (mkOccName, mkVarOcc)
 import OriginalCoreAST.CoreMakerFunctions(fractionalToCoreLiteral, integerToCoreLiteral, rationalToCoreExpression, integerToCoreExpression, stringToCoreExpression, boolToExpression)
 import OriginalCoreAST.CoreInformationExtractorFunctions(showVarExpression, varToString, nameToString, coreLiteralToFractional, isInHeadNormalForm, isTypeInformation, canBeReduced)
 import OriginalCoreAST.CoreStepperHelpers.CoreEvaluator(applyFunctionToArguments)
+import OriginalCoreAST.CoreStepperHelpers.CoreTransformator(convertFunctionApplicationWithArgumentListToNestedFunctionApplication, deepReplaceVarWithinExpression, deepReplaceVarWithinAlternative, deepReplaceMultipleVarWithinExpression, convertToMultiArgumentFunction)
+import OriginalCoreAST.CoreStepperHelpers.CoreLookup(tryFindBinding, findMatchingPattern)
 
 type ReductionStepDescription = String --for example: "replace x with definition"
 type Binding = (Var, Expr Var)
@@ -83,56 +85,3 @@ applyStepToOneOfTheArguments bindings alreadyReducedArguments (x:xs) = if canBeR
 applyStepToOneOfTheArguments bindings alreadyReducedArguments [] = Nothing --no argument that can be reduced was found. this should not happen because this condition gets checked earlier in the code
 
 
-
-convertFunctionApplicationWithArgumentListToNestedFunctionApplication :: Expr Var -> [Expr Var] -> Expr Var
-convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression [] = expression
-convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression arguments = App (convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression (init arguments)) (last arguments)
-
-
-tryFindBinding :: Var -> [Binding] -> Maybe (Expr Var)
-tryFindBinding key [] = Nothing
-tryFindBinding key ((var, exp):xs) = if ((==) (varToString var) (varToString key))
-                                                    then Just (exp)
-                                                    else tryFindBinding key xs
-
-deepReplaceVarWithinExpression :: Var -> Expr Var -> Expr Var -> Expr Var
-deepReplaceVarWithinExpression name replaceExpression (Var varName) = if (==) (varToString varName) (varToString name) then replaceExpression else (Var varName)
-deepReplaceVarWithinExpression name replaceExpression (App expression argument) = App (deepReplaceVarWithinExpression name replaceExpression expression) (deepReplaceVarWithinExpression name replaceExpression argument)
-deepReplaceVarWithinExpression name replaceExpression (Lam parameter expression) =
-  if (varToString parameter) == (varToString name)
-    then Lam parameter expression --do nothing, use local lamda parameter with the same name (shadowing)
-    else Lam parameter (deepReplaceVarWithinExpression name replaceExpression expression)
-deepReplaceVarWithinExpression name replaceExpression (Case expression binding caseType alternatives) = Case (deepReplaceVarWithinExpression name replaceExpression expression) binding caseType (map (deepReplaceVarWithinAlternative name replaceExpression) alternatives)
-deepReplaceVarWithinExpression _ _ expression = expression --nothing to replace (ToDo: Let, Cast, Tick, Type, Coercion not implemented yet)
-
-deepReplaceVarWithinAlternative :: Var -> Expr Var -> Alt Var -> Alt Var 
-deepReplaceVarWithinAlternative name replaceExpression (altCon, localBoundVars, expression) = if (elem (varToString name) (map varToString localBoundVars))
-                                                                                                 then (altCon, localBoundVars, expression) --do nothing, use local parameter with the same name (shadowing)
-                                                                                                 else (altCon, localBoundVars, (deepReplaceVarWithinExpression name replaceExpression expression))
-
-deepReplaceMultipleVarWithinExpression :: [Var] -> [Expr Var] -> Expr Var -> Expr Var
-deepReplaceMultipleVarWithinExpression [] _ expression = expression
-deepReplaceMultipleVarWithinExpression _ [] expression = expression
-deepReplaceMultipleVarWithinExpression (x:xs) (y:ys) expression = deepReplaceMultipleVarWithinExpression xs ys (deepReplaceVarWithinExpression x y expression)
-
-findMatchingPattern :: Expr Var -> [Alt Var] -> Maybe (Expr Var)
-findMatchingPattern expression [] = Nothing 
-findMatchingPattern _ ((DEFAULT, _, expression):_) = Just expression
-findMatchingPattern (Var name) (((DataAlt dataCon), _, expression):xs) = if ((==) (varToString name) (showOutputable dataCon)) --check: is there a more elegant way than "show outputable"
-                                                                                then Just expression
-                                                                                else (findMatchingPattern (Var name) xs)
-findMatchingPattern (Lit literal) (((LitAlt patternLiteral), _, expression):xs) = if ((==) literal patternLiteral) --can we compare two literals like this?
-                                                                                             then Just expression
-                                                                                             else (findMatchingPattern (Lit literal) xs)
-findMatchingPattern (App expr argument) (((DataAlt patternConstructorName), boundNames, expression):xs) = do
-    let (function, arguments) = convertToMultiArgumentFunction (App expr argument)
-    if ((==) (showVarExpression function) (showOutputable patternConstructorName)) --check: is there a more elegant way than "show outputable"
-        then Just (deepReplaceMultipleVarWithinExpression boundNames (filter (not.isTypeInformation) arguments) expression)
-        else (findMatchingPattern (App expr argument) xs) 
-findMatchingPattern expression (x:xs) = findMatchingPattern expression xs
-
-
---core utilities 
-
-convertToMultiArgumentFunction :: Expr Var -> (Expr Var, [Expr Var])
-convertToMultiArgumentFunction expr = collectArgs expr
