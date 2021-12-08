5f34667d852c034896dfbdb7caefa7664438eda9
Jan Huber <jan.huber@ost.ch>
2021-12-08 13:09:22 +0100

    add more integration tests

--- src/IntegrationTestBindings.hs
+++ src/IntegrationTestBindings.hs
@@ -1,7 +1,127 @@
 module IntegrationTestBindings where
+import Data.Maybe
+import Data.Either
 
+{-Arithmetic-}
 additionInput = 1+1
 additionExpectedOutput = 2
 
-simpleAdditionInput = 1+1
-simpleAdditionExpectedOutput = 2
+additionWithApplicationSyntaxInput = (+) 1 1
+additionWithApplicationSyntaxExpectedOutput = 2
+
+substractionInput = 1-1
+substractionExpectedOutput = 0
+
+nestedArithmeticInput = (1 + 10) - (2 * 3)
+nestedArithmeticExpectedOutput = 5
+
+
+doubleAdditionInput = 1.5 + 1.5 
+doubleAdditionExpectedOutput = 3
+
+{-Function Application-}
+
+
+functionApplicationInput = add 1 2
+functionApplicationExpectedOutput = 3
+
+
+lamdaApplicationInput = (\x y -> x+y) 1 2
+lamdaApplicationExpectedOutput = 3
+
+
+nestedApplicationInput = add (add 0 1) (add 0 2)
+nestedApplicationExpectedOutput = 3
+
+{-Higher Order Functions-}
+
+
+higherOrderResultInput = (multiplicator 10) 5
+higherOrderResultExpectedOutput = 50
+
+
+higherOrderParameterInput = twice double 1
+higherOrderParameterExpectedOutput = 4
+
+{-basic operations on Boolean Type-}
+
+basicOperationOnBooleanInput = True == False
+basicOperationOnBooleanExpectedOutput = False
+
+{-basic operations on Char Type -}
+
+basicOperationOnCharInput = succ 'a'
+basicOperationOnCharExpectedOutput = 'b' :: Char
+
+{-basic operations on Double Type-}
+
+
+basicOperationOnDoubleInput = 1.3 - 0.3
+basicOperationOnDoubleExpectedOutput = 1.0
+
+
+{-basic operations on Int Type-}
+
+basicOperationOnIntInput = (2 :: Int) - (1 :: Int)
+basicOperationOnIntExpectedOutput = 1 :: Int
+
+{-basic operations on Integer Type-}
+
+
+basicOperationOnIntegerInput = (2 :: Integer) - (1 :: Integer)
+basicOperationOnIntegerExpectedOutput = 1 :: Integer
+
+{-basic operations on Either Type-}
+
+
+
+basicOperationOnEitherInput = fromRight (Right 4)
+basicOperationOnEitherExpectedOutput = 4
+
+{-basic operations on Maybe Type-}
+
+
+basicOperationOnMaybeInput = isNothing (Just 5)
+basicOperationOnMaybeExpectedOutput = False
+
+{-basic operations on Rational Type-}
+
+
+basicOperationOnRationalInput = (2 :: Rational) > (1 :: Rational) 
+basicOperationOnRationalExpectedOutput = True
+
+{-basic operations on String Type-}
+
+basicOperationOnStringInput = "Hallo" < "World"
+basicOperationOnStringExpectedOutput = True
+
+{-basic operations on List Type-}
+
+
+basicOperationOnListInput = [1, 2, 3] < [4, 5, 6]
+basicOperationOnListExpectedOutput = True
+
+{-basic operations on Tuple Type-}
+
+
+
+basicOperationOnTupleInput = (1, 2) <  (3, 4)
+basicOperationOnTupleExpectedOutput = True
+
+{-Helper Functions-}
+
+add :: Int -> Int -> Int 
+add x y = x + y
+
+addForInteger :: Integer -> Integer -> Integer 
+addForInteger x y = x + y
+
+double :: Int -> Int 
+double x = 2*x
+
+twice :: (Int -> Int) -> Int -> Int 
+twice function number = function (function number)
+
+multiplicator :: Int -> (Int -> Int)
+multiplicator x = (\y -> x*y)
+
--- test/OriginalCoreAST/IntegrationTestSpec.hs
+++ test/OriginalCoreAST/IntegrationTestSpec.hs
@@ -11,6 +11,8 @@ import GHC.Core (Bind (NonRec), CoreProgram, Expr)
 import GHC.Types.Var (Var (varName, varType))
 import OriginalCoreAST.CoreStepperHelpers.CoreLookup(findBindingForString)
 import OriginalCoreAST.CoreStepper(reduceToHeadNormalForm)
+import OriginalCoreAST.CoreStepperHelpers.CoreEvaluator(prepareExpressionArgumentForEvaluation)
+
 type Binding = (Var, Expr Var)
 
 
@@ -20,10 +22,61 @@ spec = before getBindingFinderWithCoreBindings $ do
     describe "Arithmetic" $ do
         it "can reduce adition" $ \(bindingFinder, coreBindings) -> do
             expectationForExpression "addition" bindingFinder coreBindings
+        it "can reduce adition with application syntax" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "additionWithApplicationSyntax" bindingFinder coreBindings
+        it "can reduce substraction" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "substraction" bindingFinder coreBindings
+        it "can reduce nested arithmetic" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "nestedArithmetic" bindingFinder coreBindings
+        it "can reduce double addition" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "doubleAddition" bindingFinder coreBindings
+    describe "Basic Function/Lamda Application" $ do   
+        it "can reduce function application" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "functionApplication" bindingFinder coreBindings
+        it "can reduce lamda application" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "lamdaApplication" bindingFinder coreBindings
+        it "can reduce nested application" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "nestedApplication" bindingFinder coreBindings
+    describe "Higher Order Function/Lamda Application" $ do     
+        it "can reduce function application with higher order result" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "higherOrderResult" bindingFinder coreBindings
+        it "can reduce function application with higher order parameter" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "higherOrderParameter" bindingFinder coreBindings
+    describe "Types" $ do        
+        it "can reduce basic operation on Boolean" $ \(bindingFinder, coreBindings) -> do
+            --expectationForExpression "basicOperationOnBoolean" bindingFinder coreBindings
+            pendingWith "Boolean type not fully supported"
+        it "can reduce basic operation on Char" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnChar" bindingFinder coreBindings
+        it "can reduce basic operation on Double" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnDouble" bindingFinder coreBindings
+        it "can reduc basic operation on Int" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnInt" bindingFinder coreBindings
+        it "can reduce basic operation on Integer" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnInteger" bindingFinder coreBindings
+        it "can reduce basic operation on Either" $ \(bindingFinder, coreBindings) -> do
+            --expectationForExpression "basicOperationOnEither" bindingFinder coreBindings
+            pendingWith "Either type not fully supported"
+        it "can reduce basic operation on Maybe" $ \(bindingFinder, coreBindings) -> do
+            --expectationForExpression "basicOperationOnMaybe" bindingFinder coreBindings
+            pendingWith "Maybe type not fully supported"
+        it "can reduce basic operation on  Rational" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnRational" bindingFinder coreBindings
+        it "can reduce basic operation on String" $ \(bindingFinder, coreBindings) -> do
+            expectationForExpression "basicOperationOnString" bindingFinder coreBindings
+        it "can reduce basic operation on List" $ \(bindingFinder, coreBindings) -> do
+            --expectationForExpression "basicOperationOnList" bindingFinder coreBindings
+            pendingWith "List type not fully supported"
+        it "can reduce basic operation on Tuple" $ \(bindingFinder, coreBindings) -> do
+            --expectationForExpression "basicOperationOnTuple" bindingFinder coreBindings
+            pendingWith "Tuple type not fully supported"
+
 
 expectationForExpression :: String -> (String -> Expr Var) -> [Binding] -> Expectation
 expectationForExpression expressionBindingName bindingFinder coreBindings = do
         let input = (bindingFinder (expressionBindingName ++ "Input"))
         let expectedOutput = (bindingFinder(expressionBindingName ++ "ExpectedOutput"))
         let output = reduceToHeadNormalForm coreBindings input
-        output `shouldBe` expectedOutput
+        (prepareExpressionArgumentForEvaluation output) `shouldBe` (prepareExpressionArgumentForEvaluation expectedOutput)
+
+
