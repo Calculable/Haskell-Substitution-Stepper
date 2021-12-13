8283d61f3a6ea824d1e3b757fd33efff0012df0b
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:58:12 +0100

    clean up stepper printer

--- src/OriginalCoreAST/CoreStepperPrinter.hs
+++ src/OriginalCoreAST/CoreStepperPrinter.hs
@@ -1,48 +1,49 @@
 module OriginalCoreAST.CoreStepperPrinter
-  (printCoreStepByStepReductionForBinding, printCoreStepByStepReductionForEveryBinding
+  ( printCoreStepByStepReductionForBinding,
+    printCoreStepByStepReductionForEveryBinding,
   )
 where
 
-import OriginalCoreAST.CoreInformationExtractorFunctions(varToString, canBeReduced)
-import OriginalCoreAST.CoreStepper(applyStep)
-import Data.Maybe
-import GHC.Core (Bind (NonRec, Rec), Expr (..), Alt, AltCon (..), CoreBind, collectArgs)
-import GHC.Types.Literal( Literal (LitChar, LitDouble, LitFloat, LitNumber, LitString), mkLitInt64, mkLitString)
-
-import GHC.Types.Var (Var (varName, varType), TyVar, Id, mkCoVar, mkGlobalVar)
-import OriginalCoreAST.CorePrettyPrinter(prettyPrint)
+import GHC.Plugins (Bind (NonRec, Rec), CoreBind, Expr, Var)
+import OriginalCoreAST.CoreInformationExtractorFunctions
+  ( canBeReduced,
+    varToString,
+  )
+import OriginalCoreAST.CorePrettyPrinter (prettyPrint)
+import OriginalCoreAST.CoreStepper (applyStep)
 
 type ReductionStepDescription = String --for example: "replace x with definition"
+
 type Binding = (Var, Expr Var)
 
-printCoreStepByStepReductionForEveryBinding :: [CoreBind] -> IO()
+printCoreStepByStepReductionForEveryBinding :: [CoreBind] -> IO ()
 printCoreStepByStepReductionForEveryBinding bindings = do
-    let allBindings = convertToBindingsList bindings
-    mapM_ (printCoreStepByStepReductionForBinding allBindings) allBindings
+  let allBindings = convertToBindingsList bindings
+  mapM_ (printCoreStepByStepReductionForBinding allBindings) allBindings
 
 printCoreStepByStepReductionForBinding :: [Binding] -> Binding -> IO ()
 printCoreStepByStepReductionForBinding bindings (var, exp) = do
-    putStr "\n**Reduction of "
-    putStr (varToString var)
-    putStr "**"
-    putStrLn ""
-    prettyPrint exp
-    printCoreStepByStepReductionForSingleExpression bindings exp
+  putStr "\n**Reduction of "
+  putStr (varToString var)
+  putStr "**"
+  putStrLn ""
+  prettyPrint exp
+  printCoreStepByStepReductionForSingleExpression bindings exp
 
-printCoreStepByStepReductionForSingleExpression :: [Binding] -> Expr Var -> IO()
-printCoreStepByStepReductionForSingleExpression bindings expression     | canBeReduced expression = do
-                                                                            let reduction = (applyStep bindings expression)
-                                                                            case reduction of
-                                                                                Just (reductionStepDescription, reducedExpression) -> do
-                                                                                    putStrLn ("\n{-" ++ reductionStepDescription ++ "-}")
-                                                                                    prettyPrint reducedExpression
-                                                                                    printCoreStepByStepReductionForSingleExpression bindings reducedExpression
-                                                                                Nothing -> putStrLn "\n{-no reduction rule implemented for this expression-}"
-                                                                        | otherwise = putStrLn "\n{-reduction complete-}"
-
+printCoreStepByStepReductionForSingleExpression :: [Binding] -> Expr Var -> IO ()
+printCoreStepByStepReductionForSingleExpression bindings expression
+  | canBeReduced expression = do
+    let reduction = applyStep bindings expression
+    case reduction of
+      Just (reductionStepDescription, reducedExpression) -> do
+        putStrLn ("\n{-" ++ reductionStepDescription ++ "-}")
+        prettyPrint reducedExpression
+        printCoreStepByStepReductionForSingleExpression bindings reducedExpression
+      Nothing -> putStrLn "\n{-no reduction rule implemented for this expression-}"
+  | otherwise = putStrLn "\n{-reduction complete-}"
 
 convertToBindingsList :: [CoreBind] -> [Binding]
-convertToBindingsList bindings = concat (map convertCoreBindingToBindingList bindings)
+convertToBindingsList = concatMap convertCoreBindingToBindingList
 
 convertCoreBindingToBindingList :: CoreBind -> [Binding]
 convertCoreBindingToBindingList (NonRec binding exp) = [(binding, exp)]
