b080224eb17715e4f45af7c66ead46658a5e1af1
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:32:00 +0100

    move examples to dedicated dir

---
+++ test/examples/GADTSourceExample.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE GADTs #-}
+
+module GADTSourceExample where
+
+data Expr a where
+  ILit :: Int -> Expr Int
+  BLit :: Bool -> Expr Bool
+  AddExpr :: Expr Int -> Expr Int -> Expr Int
+  EqExpr :: Eq a => Expr a -> Expr a -> Expr Bool
+
+evalExpr :: Expr a -> a
+evalExpr (ILit x) = x
+evalExpr (BLit x) = x
+evalExpr (AddExpr a b) = evalExpr a + evalExpr b
+evalExpr (EqExpr a b) = evalExpr a == evalExpr b
---
+++ test/examples/SafeListSouceExample.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE GADTs #-}
+
+module SafeListSouceExample where
+
+data Empty
+data NonEmpty
+
+data SafeList a b where
+    Nil :: SafeList a Empty
+    Cons :: a -> SafeList a b -> SafeList a NonEmpty
+
+instance Show a => Show (SafeList a b) where
+    show Nil = "Nil"
+    show (Cons x xs) = show x ++ ", " ++ show xs
+
+safeHead :: SafeList a NonEmpty -> a
+safeHead (Cons x _) = x
