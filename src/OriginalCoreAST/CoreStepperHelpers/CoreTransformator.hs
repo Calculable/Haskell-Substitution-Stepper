a2be60977038ef80db736be3e4122813a06f9e97
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:51:27 +0100

    clean up transformator

--- src/OriginalCoreAST/CoreStepperHelpers/CoreTransformator.hs
+++ src/OriginalCoreAST/CoreStepperHelpers/CoreTransformator.hs
@@ -1,25 +1,27 @@
-module OriginalCoreAST.CoreStepperHelpers.CoreTransformator(convertFunctionApplicationWithArgumentListToNestedFunctionApplication, deepReplaceVarWithinExpression, deepReplaceVarWithinAlternative, deepReplaceMultipleVarWithinExpression, convertToMultiArgumentFunction)
-where
+module OriginalCoreAST.CoreStepperHelpers.CoreTransformator (convertFunctionApplicationWithArgumentListToNestedFunctionApplication, deepReplaceVarWithinExpression, deepReplaceVarWithinAlternative, deepReplaceMultipleVarWithinExpression, convertToMultiArgumentFunction) where
 
-import OriginalCoreAST.CoreTypeClassInstances ()
-import Data.Maybe ()
-import GHC.Core (Bind (NonRec, Rec), Expr (..), Alt, AltCon (..), CoreBind, collectArgs)
-import GHC.Types.Literal
-  ( Literal (LitChar, LitDouble, LitFloat, LitNumber, LitString), mkLitInt64, mkLitString
+import GHC.Plugins
+  ( Alt,
+    Bind (..),
+    Expr (App, Case, Lam, Let, Var),
+    Var,
+    collectArgs,
   )
-import GHC.Types.Var (Var (varName, varType), TyVar, Id, mkCoVar, mkGlobalVar)
-import OriginalCoreAST.CoreMakerFunctions(fractionalToCoreLiteral, integerToCoreLiteral, rationalToCoreExpression, integerToCoreExpression, stringToCoreExpression, boolToCoreExpression)
-import OriginalCoreAST.CoreInformationExtractorFunctions(varExpressionToString, varToString, nameToString, coreLiteralToFractional, isInHeadNormalForm, isTypeInformation, canBeReduced)
+import OriginalCoreAST.CoreInformationExtractorFunctions
+  ( varToString,
+  )
+import OriginalCoreAST.CoreMakerFunctions ()
+import OriginalCoreAST.CoreTypeClassInstances ()
 
 convertFunctionApplicationWithArgumentListToNestedFunctionApplication :: Expr Var -> [Expr Var] -> Expr Var
 convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression [] = expression
 convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression arguments = App (convertFunctionApplicationWithArgumentListToNestedFunctionApplication expression (init arguments)) (last arguments)
 
 deepReplaceVarWithinExpression :: Var -> Expr Var -> Expr Var -> Expr Var
-deepReplaceVarWithinExpression name replaceExpression (Var varName) = if (==) (varToString varName) (varToString name) then replaceExpression else (Var varName)
+deepReplaceVarWithinExpression name replaceExpression (Var varName) = if (==) (varToString varName) (varToString name) then replaceExpression else Var varName
 deepReplaceVarWithinExpression name replaceExpression (App expression argument) = App (deepReplaceVarWithinExpression name replaceExpression expression) (deepReplaceVarWithinExpression name replaceExpression argument)
 deepReplaceVarWithinExpression name replaceExpression (Lam parameter expression) =
-  if (varToString parameter) == (varToString name)
+  if varToString parameter == varToString name
     then Lam parameter expression --do nothing, use local lamda parameter with the same name (shadowing)
     else Lam parameter (deepReplaceVarWithinExpression name replaceExpression expression)
 deepReplaceVarWithinExpression name replaceExpression (Case expression binding caseType alternatives) = Case (deepReplaceVarWithinExpression name replaceExpression expression) binding caseType (map (deepReplaceVarWithinAlternative name replaceExpression) alternatives)
@@ -27,25 +29,27 @@ deepReplaceVarWithinExpression name replaceExpression (Let binding expression) =
 deepReplaceVarWithinExpression _ _ expression = expression --nothing to replace (ToDo: Cast and Coercion not implemented yet)
 
 deepReplaceVarWithinAlternative :: Var -> Expr Var -> Alt Var -> Alt Var
-deepReplaceVarWithinAlternative name replaceExpression (altCon, localBoundVars, expression) = if elem (varToString name) (map varToString localBoundVars)
-                                                                                                 then (altCon, localBoundVars, expression) --do nothing, use local parameter with the same name (shadowing)
-                                                                                                 else (altCon, localBoundVars, deepReplaceVarWithinExpression name replaceExpression expression)
+deepReplaceVarWithinAlternative name replaceExpression (altCon, localBoundVars, expression) =
+  if varToString name `elem` map varToString localBoundVars
+    then (altCon, localBoundVars, expression) --do nothing, use local parameter with the same name (shadowing)
+    else (altCon, localBoundVars, deepReplaceVarWithinExpression name replaceExpression expression)
 
 deepReplaceVarWithinBinding :: Var -> Expr Var -> Bind Var -> Bind Var
 deepReplaceVarWithinBinding name replaceExpression (NonRec b expression) = NonRec b newExpression
-                                                                              where newExpression = snd (deepReplaceVarWithinBindingTuple name replaceExpression (b, expression))
+  where
+    newExpression = snd (deepReplaceVarWithinBindingTuple name replaceExpression (b, expression))
 deepReplaceVarWithinBinding name replaceExpression (Rec bindings) = Rec (map (deepReplaceVarWithinBindingTuple name replaceExpression) bindings)
 
 deepReplaceVarWithinBindingTuple :: Var -> Expr Var -> (Var, Expr Var) -> (Var, Expr Var)
-deepReplaceVarWithinBindingTuple name replaceExpression (b, expression) = if (==) (varToString b) (varToString name)
-                                                                                then (b, expression) --do nothing (shadowing)
-                                                                                else (b, deepReplaceVarWithinExpression name replaceExpression expression)
+deepReplaceVarWithinBindingTuple name replaceExpression (b, expression) =
+  if (==) (varToString b) (varToString name)
+    then (b, expression) --do nothing (shadowing)
+    else (b, deepReplaceVarWithinExpression name replaceExpression expression)
 
 deepReplaceMultipleVarWithinExpression :: [Var] -> [Expr Var] -> Expr Var -> Expr Var
 deepReplaceMultipleVarWithinExpression [] _ expression = expression
 deepReplaceMultipleVarWithinExpression _ [] expression = expression
-deepReplaceMultipleVarWithinExpression (x:xs) (y:ys) expression = deepReplaceMultipleVarWithinExpression xs ys (deepReplaceVarWithinExpression x y expression)
+deepReplaceMultipleVarWithinExpression (x : xs) (y : ys) expression = deepReplaceMultipleVarWithinExpression xs ys (deepReplaceVarWithinExpression x y expression)
 
 convertToMultiArgumentFunction :: Expr Var -> (Expr Var, [Expr Var])
 convertToMultiArgumentFunction = collectArgs
-
