e43a9aef5eab41fb6cd20065b6b4d97ae44c3e86
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:32:17 +0100

    remove ast example

--- src/ast-example/compiler-steps.sh
+++
@@ -1,7 +0,0 @@
-# For less output use:
-# -dsuppress-all
-# -dsuppress-idinfo
-
-stack ghc -- Main.hs -dsuppress-timestamps -dsuppress-all -ddump-to-file -ddump-parsed-ast -ddump-rn-ast -ddump-tc-ast -ddump-ds-preopt -ddump-stg -ddump-cmm-raw -ddump-asm-native 
-
-rm Main.hi Main.o
