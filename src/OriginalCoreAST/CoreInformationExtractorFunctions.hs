6a59ab782b8cfbfcf738459554298c2d691552d4
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:52:54 +0100

    clean up extractor

--- src/OriginalCoreAST/CoreInformationExtractorFunctions.hs
+++ src/OriginalCoreAST/CoreInformationExtractorFunctions.hs
@@ -1,12 +1,14 @@
-module OriginalCoreAST.CoreInformationExtractorFunctions(varExpressionToString, varToString, nameToString, coreLiteralToFractional, isInHeadNormalForm, isTypeInformation, canBeReduced)
-where
+module OriginalCoreAST.CoreInformationExtractorFunctions (varExpressionToString, varToString, nameToString, coreLiteralToFractional, isInHeadNormalForm, isTypeInformation, canBeReduced) where
 
-import GHC.Core (Expr (..))
-import GHC.Types.Literal(Literal (..), mkLitInt64, mkLitString)
-import GHC.Types.Var (Var (varName, varType), TyVar, Id, mkCoVar, mkGlobalVar)
-import GHC.Types.Name(nameUnique, Name, mkSystemVarName, mkSysTvName, mkSystemName, pprNameUnqualified, nameStableString, getOccString)
-import GHC.Core.Utils (exprIsHNF)
-import Data.List(isPrefixOf)
+import Data.List (isPrefixOf)
+import GHC.Plugins
+  ( Expr (Type, Var),
+    Literal (LitDouble, LitFloat),
+    Name,
+    Var (varName),
+    exprIsHNF,
+    getOccString,
+  )
 
 varExpressionToString :: Expr Var -> String
 varExpressionToString (Var var) = varToString var
@@ -27,15 +29,16 @@ isInHeadNormalForm = exprIsHNF
 
 isTypeInformation :: Expr Var -> Bool
 isTypeInformation (Type _) = True
-isTypeInformation (Var name) = "$" `isPrefixOf` (varToString name)
+isTypeInformation (Var name) = "$" `isPrefixOf` varToString name
 isTypeInformation x = False
 
 -- | The "canBeReducedFunction" checks if a Core expression can be reduced.
+canBeReduced :: Expr Var -> Bool
 canBeReduced exp
   | isTypeInformation exp = False
   | isBooleanVar exp = False
   | otherwise = not (exprIsHNF exp)
 
 isBooleanVar :: Expr Var -> Bool
-isBooleanVar (Var x) = or [((==) (varToString x) "True"), ((==) (varToString x) "False")]
+isBooleanVar (Var x) = (==) (varToString x) "True" || (==) (varToString x) "False"
 isBooleanVar _ = False
