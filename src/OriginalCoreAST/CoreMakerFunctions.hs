58e92091bae614eff6345ece23df2563bf2960b3
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:54:02 +0100

    clean up core maker functions

--- src/OriginalCoreAST/CoreMakerFunctions.hs
+++ src/OriginalCoreAST/CoreMakerFunctions.hs
@@ -1,21 +1,26 @@
-module OriginalCoreAST.CoreMakerFunctions(fractionalToCoreLiteral, integerToCoreLiteral, rationalToCoreExpression, integerToCoreExpression, stringToCoreExpression, boolToCoreExpression, charToCoreLiteral, rationalToCoreLiteral)
-where
+module OriginalCoreAST.CoreMakerFunctions (fractionalToCoreLiteral, integerToCoreLiteral, rationalToCoreExpression, integerToCoreExpression, stringToCoreExpression, boolToCoreExpression, charToCoreLiteral, rationalToCoreLiteral) where
 
-import GHC.Core (Expr (..))
-import GHC.Types.Literal(Literal (..), mkLitInt64, mkLitString)
-import GHC.Types.Var (Var (varName, varType), TyVar, Id, mkCoVar, mkGlobalVar)
-import GHC.Types.Name(nameUnique, Name, mkSystemVarName, mkSysTvName, mkSystemName, pprNameUnqualified, nameStableString, getOccString)
+import GHC.Core.TyCo.Rep (TyLit (StrTyLit), Type (LitTy))
+import GHC.Plugins
+  ( Expr (Lit, Var),
+    IdDetails (VanillaId),
+    Literal (LitChar, LitDouble),
+    Var,
+    mkFastString,
+    mkGlobalVar,
+    mkLitInt64,
+    mkLitString,
+    mkSystemName,
+    mkVarOcc,
+    vanillaIdInfo,
+  )
 import GHC.Types.Unique (minLocalUnique)
-import GHC.Data.FastString (mkFastString)
-import GHC.Core.TyCo.Rep (Type(..), TyLit(..))
-import GHC.Types.Id.Info ( vanillaIdInfo, IdDetails(..))
-import GHC.Types.Name.Occurrence (mkOccName, mkVarOcc)
 
 integerToCoreLiteral :: Integer -> Literal
 integerToCoreLiteral = mkLitInt64
 
 fractionalToCoreLiteral :: Real a => a -> Literal
-fractionalToCoreLiteral value = (LitDouble (toRational value))
+fractionalToCoreLiteral value = LitDouble (toRational value)
 
 charToCoreLiteral :: Char -> Literal
 charToCoreLiteral = LitChar
@@ -26,13 +31,12 @@ rationalToCoreExpression value = Lit (rationalToCoreLiteral value)
 rationalToCoreLiteral :: Rational -> Literal
 rationalToCoreLiteral = LitDouble
 
-
 integerToCoreExpression :: Integer -> Expr b
 integerToCoreExpression value = Lit (integerToCoreLiteral value)
 
 stringToCoreExpression :: String -> Expr Var
 stringToCoreExpression value = Lit (mkLitString value)
 
-boolToCoreExpression :: Bool -> Expr Var  --this is a hack, we just took the easiest constructors we found to create a "Var"-Instance without understanding what those constructors stand for 
-boolToCoreExpression True = Var (mkGlobalVar VanillaId (mkSystemName minLocalUnique (mkVarOcc ("True"))) (LitTy (StrTyLit (mkFastString "Bool"))) vanillaIdInfo)
-boolToCoreExpression False = Var (mkGlobalVar VanillaId (mkSystemName minLocalUnique (mkVarOcc ("False"))) (LitTy (StrTyLit (mkFastString "Bool"))) vanillaIdInfo)
+boolToCoreExpression :: Bool -> Expr Var --this is a hack, we just took the easiest constructors we found to create a "Var"-Instance without understanding what those constructors stand for
+boolToCoreExpression True = Var (mkGlobalVar VanillaId (mkSystemName minLocalUnique (mkVarOcc "True")) (LitTy (StrTyLit (mkFastString "Bool"))) vanillaIdInfo)
+boolToCoreExpression False = Var (mkGlobalVar VanillaId (mkSystemName minLocalUnique (mkVarOcc "False")) (LitTy (StrTyLit (mkFastString "Bool"))) vanillaIdInfo)
