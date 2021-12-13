e4210a4a520de30f410e437806356158e0b09416
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:43:57 +0100

    clean up flat printer

--- src/FlatCoreASTPrinter.hs
+++ src/FlatCoreASTPrinter.hs
@@ -1,47 +1,47 @@
 module FlatCoreASTPrinter
-  (printFlatCoreAST
+  ( printFlatCoreAST,
   )
 where
 
-import GHC.Core (Bind (NonRec, Rec), Expr (..), Alt, AltCon)
-import GHC.Types.Literal
-  ( Literal (LitChar, LitDouble, LitFloat, LitNumber, LitString),
+import Data.List (isPrefixOf)
+import GHC.Plugins
+  ( Alt,
+    Bind (..),
+    Expr (App, Case, Lam, Let, Lit, Type, Var),
+    Literal (LitChar, LitDouble, LitFloat, LitNumber, LitString),
+    Outputable (ppr),
+    OutputableBndr,
+    Var (varName),
   )
-import GHC.Types.Var (Var (varName, varType), TyVar, Id)
-import GHC.Utils.Outputable (Outputable (ppr), OutputableBndr)
-import Utils (printAst, showOutputable)
-import GHC.Core.Ppr
-  ( pprCoreAlt,
-  )
-import Data.List(isPrefixOf)
+import Utils (showOutputable)
 
 printFlatCoreAST :: (OutputableBndr a) => [Bind a] -> IO ()
-printFlatCoreAST x = mapM_ printFlatCoreBinding x
+printFlatCoreAST = mapM_ printFlatCoreBinding
 
 printFlatCoreBinding :: (OutputableBndr a) => Bind a -> IO ()
 printFlatCoreBinding (NonRec b exp) = printFlatBindingWithExpression (b, exp)
 printFlatCoreBinding (Rec bindings) = mapM_ printFlatBindingWithExpression bindings
 
-printFlatBindingWithExpression ::  (OutputableBndr b) => (b, Expr b) -> IO ()
+printFlatBindingWithExpression :: (OutputableBndr b) => (b, Expr b) -> IO ()
 printFlatBindingWithExpression (b, exp) = do
-    putStr (showOutputable b)
-    putStr " = "
-    printFlatCoreExpression exp
-    putStrLn ""
+  putStr (showOutputable b)
+  putStr " = "
+  printFlatCoreExpression exp
+  putStrLn ""
 
 printFlatCoreExpression :: (OutputableBndr b) => Expr b -> IO ()
 printFlatCoreExpression (Var id) = do
-    let name = (showOutputable $ varName id)
-    if (isPrefixOf "$" name)
-      then putStr ""
-      else putStr name
+  let name = showOutputable $ varName id
+  if "$" `isPrefixOf` name
+    then putStr ""
+    else putStr name
 printFlatCoreExpression (Lit lit) = do
-    case lit of
-      LitChar c -> putStr (show c)
-      LitNumber t v -> putStr (show v)
-      LitString bs -> putStr (show bs)
-      LitFloat f -> putStr (show f)
-      LitDouble d -> putStr (show d)
+  case lit of
+    LitChar c -> putStr (show c)
+    LitNumber t v -> putStr (show v)
+    LitString bs -> putStr (show bs)
+    LitFloat f -> putStr (show f)
+    LitDouble d -> putStr (show d)
 printFlatCoreExpression (App exp arg) = do
   putStr "("
   printFlatCoreExpression exp
@@ -72,11 +72,10 @@ printFlatCoreExpression (Case exp b t alts) = do
 printFlatCoreExpression x = do
   putStr "Unsupported Constructor"
 
-
 printFlatAlt :: (OutputableBndr b) => Alt b -> IO ()
 printFlatAlt (altCon, b, exp) = do
-    putStr "\n\t"
-    putStr (showOutputable altCon) --either literal or constructor or _
-    putStr (unwords ( (map showOutputable b)))
-    putStr " -> "
-    printFlatCoreExpression exp
+  putStr "\n\t"
+  putStr (showOutputable altCon) --either literal or constructor or _
+  putStr (unwords (map showOutputable b))
+  putStr " -> "
+  printFlatCoreExpression exp
