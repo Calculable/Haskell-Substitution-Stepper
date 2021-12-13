e36b8f29100fd327fc1836706155bdf7f4b934cc
Robin Elvedi <trivialic@posteo.net>
2021-12-13 16:48:21 +0100

    clean up evaluator

--- src/OriginalCoreAST/CoreStepperHelpers/CoreEvaluator.hs
+++ src/OriginalCoreAST/CoreStepperHelpers/CoreEvaluator.hs
@@ -1,26 +1,26 @@
-module OriginalCoreAST.CoreStepperHelpers.CoreEvaluator(evaluateFunctionWithArguments)
-where
+module OriginalCoreAST.CoreStepperHelpers.CoreEvaluator (evaluateFunctionWithArguments) where
 
+import GHC.Plugins (Expr (App, Lit, Var), Literal (LitNumber), Var)
+import OriginalCoreAST.CoreInformationExtractorFunctions
+  ( isTypeInformation,
+    varToString,
+  )
+import OriginalCoreAST.CoreMakerFunctions
+  ( boolToCoreExpression,
+    integerToCoreExpression,
+    rationalToCoreExpression,
+  )
 import OriginalCoreAST.CoreTypeClassInstances ()
-import Data.Maybe ()
-import GHC.Core (Expr (..))
-import GHC.Types.Literal ( Literal (..))
-import GHC.Types.Var (Var)
-import Debug.Trace(trace)
-import OriginalCoreAST.CoreMakerFunctions(fractionalToCoreLiteral, integerToCoreLiteral, rationalToCoreExpression, integerToCoreExpression, stringToCoreExpression, boolToCoreExpression)
-import OriginalCoreAST.CoreInformationExtractorFunctions(varExpressionToString, varToString, nameToString, coreLiteralToFractional, isInHeadNormalForm, isTypeInformation, canBeReduced)
 
 evaluateFunctionWithArguments :: Expr Var -> [Expr Var] -> Maybe (Expr Var)
 evaluateFunctionWithArguments (Var functionOrOperatorName) arguments = do
-    let argumentsWithoutApplications = map replaceApplicationWithArgument arguments --sometimes, arguments are of type "application". For example for a Double: "D# 0.5##". ToDo: Check if this command is dangerous
-    evaluateUnsteppableFunctionWithArguments (varToString functionOrOperatorName) (filter (not.isTypeInformation) argumentsWithoutApplications) --Precondition: function must be in the form of "var". This is already checked by the function which is calling this function.
+  let argumentsWithoutApplications = map replaceApplicationWithArgument arguments --sometimes, arguments are of type "application". For example for a Double: "D# 0.5##". ToDo: Check if this command is dangerous
+  evaluateUnsteppableFunctionWithArguments (varToString functionOrOperatorName) (filter (not . isTypeInformation) argumentsWithoutApplications) --Precondition: function must be in the form of "var". This is already checked by the function which is calling this function.
 evaluateFunctionWithArguments _ _ = error "function-expression has to be a 'Var'"
 
 replaceApplicationWithArgument :: Expr Var -> Expr Var
 replaceApplicationWithArgument (App expr arg) = arg
 replaceApplicationWithArgument x = x
-
-
 
 evaluateUnsteppableFunctionWithArguments :: String -> [Expr Var] -> Maybe (Expr Var)
 evaluateUnsteppableFunctionWithArguments "+" [x, y] = Just ((+) x y)
@@ -31,7 +31,7 @@ evaluateUnsteppableFunctionWithArguments "recip" [x] = Just (recip x)
 evaluateUnsteppableFunctionWithArguments "signum" [x] = Just (signum x)
 evaluateUnsteppableFunctionWithArguments "abs" [x] = Just (abs x)
 evaluateUnsteppableFunctionWithArguments "negate" [x] = Just (negate x)
-evaluateUnsteppableFunctionWithArguments "fromInteger" [(Lit (LitNumber _ x))] = Just (fromInteger x)
+evaluateUnsteppableFunctionWithArguments "fromInteger" [Lit (LitNumber _ x)] = Just (fromInteger x)
 evaluateUnsteppableFunctionWithArguments "/=" [x, y] = Just (boolToCoreExpression ((/=) x y))
 evaluateUnsteppableFunctionWithArguments "==" [x, y] = Just (boolToCoreExpression ((==) x y))
 evaluateUnsteppableFunctionWithArguments "<" [x, y] = Just (boolToCoreExpression ((<) x y))
@@ -43,7 +43,7 @@ evaluateUnsteppableFunctionWithArguments "max" [x, y] = Just $ max x y
 evaluateUnsteppableFunctionWithArguments "unpackCString#" [x] = Just x
 evaluateUnsteppableFunctionWithArguments "succ" [x] = Just $ succ x
 evaluateUnsteppableFunctionWithArguments "pred" [x] = Just $ pred x
-evaluateUnsteppableFunctionWithArguments "fromEnum" [x] = Just $ integerToCoreExpression  (toInteger (fromEnum x))
+evaluateUnsteppableFunctionWithArguments "fromEnum" [x] = Just $ integerToCoreExpression (toInteger (fromEnum x))
 evaluateUnsteppableFunctionWithArguments "exp" [x] = Just (exp x)
 evaluateUnsteppableFunctionWithArguments "log" [x] = Just (log x)
 evaluateUnsteppableFunctionWithArguments "sqrt" [x] = Just (sqrt x)
@@ -69,10 +69,10 @@ evaluateUnsteppableFunctionWithArguments "toInteger" [x] = Just (integerToCoreEx
 evaluateUnsteppableFunctionWithArguments "toRational" [x] = Just (rationalToCoreExpression (toRational x))
 evaluateUnsteppableFunctionWithArguments "floatRadix" [x] = Just $ integerToCoreExpression (floatRadix x)
 evaluateUnsteppableFunctionWithArguments "floatDigits" [x] = Just $ integerToCoreExpression (toInteger (floatDigits x))
-evaluateUnsteppableFunctionWithArguments "encodeFloat" [(Lit (LitNumber _ x)), (Lit (LitNumber _ y))] = Just (encodeFloat x (fromIntegral y))
+evaluateUnsteppableFunctionWithArguments "encodeFloat" [Lit (LitNumber _ x), Lit (LitNumber _ y)] = Just (encodeFloat x (fromIntegral y))
 evaluateUnsteppableFunctionWithArguments "exponent" [x] = Just (integerToCoreExpression (toInteger (exponent x)))
 evaluateUnsteppableFunctionWithArguments "significand" [x] = Just (significand x)
-evaluateUnsteppableFunctionWithArguments "scaleFloat" [(Lit (LitNumber _ x)), y] = Just (scaleFloat (fromIntegral x) y)
+evaluateUnsteppableFunctionWithArguments "scaleFloat" [Lit (LitNumber _ x), y] = Just (scaleFloat (fromIntegral x) y)
 evaluateUnsteppableFunctionWithArguments "isNaN" [x] = Just (boolToCoreExpression (isNaN x))
 evaluateUnsteppableFunctionWithArguments "isInfinite" [x] = Just (boolToCoreExpression (isInfinite x))
 evaluateUnsteppableFunctionWithArguments "isDenormalized" [x] = Just (boolToCoreExpression (isDenormalized x))
@@ -83,6 +83,5 @@ evaluateUnsteppableFunctionWithArguments "truncate" [x] = Just (integerToCoreExp
 evaluateUnsteppableFunctionWithArguments "round" [x] = Just (integerToCoreExpression (toInteger (round x)))
 evaluateUnsteppableFunctionWithArguments "ceiling" [x] = Just (integerToCoreExpression (toInteger (ceiling x)))
 evaluateUnsteppableFunctionWithArguments "floor" [x] = Just (integerToCoreExpression (toInteger (floor x)))
-
 evaluateUnsteppableFunctionWithArguments name _ = Nothing --function not supported
 --toDo: Implement more operators and functions
