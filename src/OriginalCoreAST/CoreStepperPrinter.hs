ae4f78e4242c16f1acdb9a63ff35660613957a69
Jan Huber <jan.huber@ost.ch>
2021-12-21 09:52:41 +0100

    CLI can pass output configuration object

--- src/OriginalCoreAST/CoreStepperPrinter.hs
+++ src/OriginalCoreAST/CoreStepperPrinter.hs
@@ -26,24 +26,24 @@ import OriginalCoreAST.CoreTypeDefinitions
 
 
 -- |takes a list of core bindings and shows a step-by-step reduction for each binding
-printCoreStepByStepReductionForEveryBinding :: PrintingStyle -> [CoreBind] -> IO ()
-printCoreStepByStepReductionForEveryBinding printingStyle bindings = do
+printCoreStepByStepReductionForEveryBinding :: StepperOutputConfiguration -> [CoreBind] -> IO ()
+printCoreStepByStepReductionForEveryBinding configuration bindings = do
   let allBindings = convertToBindingsList bindings
-  mapM_ (printCoreStepByStepReductionForBinding printingStyle allBindings) allBindings
+  mapM_ (printCoreStepByStepReductionForBinding configuration allBindings) allBindings
 
 -- |takes a single core binding and shows a step-by-step reduction of the expression
-printCoreStepByStepReductionForBinding :: PrintingStyle -> [Binding] -> Binding -> IO ()
-printCoreStepByStepReductionForBinding printingStyle bindings (var, exp) = do
+printCoreStepByStepReductionForBinding :: StepperOutputConfiguration -> [Binding] -> Binding -> IO ()
+printCoreStepByStepReductionForBinding configuration bindings (var, exp) = do
   putStr "\n**Reduction of "
   putStr (varToString var)
   putStr "**"
   putStrLn ""
-  prettyPrint printingStyle exp
-  printCoreStepByStepReductionForSingleExpression printingStyle bindings exp
+  prettyPrint (printingStyle configuration) exp
+  printCoreStepByStepReductionForSingleExpression configuration bindings exp
 
 -- |takes a core expression and shows a step-by-step reduction
-printCoreStepByStepReductionForSingleExpression :: PrintingStyle -> [Binding] -> CoreExpr -> IO ()
-printCoreStepByStepReductionForSingleExpression printingStyle bindings expression = do
+printCoreStepByStepReductionForSingleExpression :: StepperOutputConfiguration -> [Binding] -> CoreExpr -> IO ()
+printCoreStepByStepReductionForSingleExpression configuration bindings expression = do
   let (stepResults, reductionSuccessfulFlag) = getAllSteps bindings expression
 
   let stepResultsExceptLast = if null stepResults
@@ -51,26 +51,24 @@ printCoreStepByStepReductionForSingleExpression printingStyle bindings expressio
                                 else init stepResults
   let stepResultsLast = ([last stepResults | not (null stepResults)])
 
-  let filteredStepResults = (filter (\(stepDescription, _, _) -> not (shouldFilterOutReductionStep printingStyle stepDescription)) stepResultsExceptLast) ++ stepResultsLast 
-  printStepResultList 0 printingStyle filteredStepResults reductionSuccessfulFlag
+  let filteredStepResults = (filter (\(stepDescription, _, _) -> not (shouldFilterOutReductionStepForPrintingStyle (printingStyle configuration) stepDescription)) stepResultsExceptLast) ++ stepResultsLast 
+  printStepResultList 0 configuration filteredStepResults reductionSuccessfulFlag
 
-printStepResultList :: Int -> PrintingStyle -> [StepResult] -> ReductionSuccessfulFlag -> IO ()
+printStepResultList :: Int -> StepperOutputConfiguration -> [StepResult] -> ReductionSuccessfulFlag -> IO ()
 printStepResultList _ _ [] True = putStrLn "{- reduction completed successfully -}"
 printStepResultList _ _ [] False = putStrLn "{- reduction completed: no reduction rule implemented for this expression -}"
-printStepResultList amountOfSkippedSteps printingStyle ((stepDescription, expression, _):xs) successFlat = do
+printStepResultList amountOfSkippedSteps configuration ((stepDescription, expression, _):xs) successFlat = do
   
-  
-  
-  if shouldShowReductionStep stepDescription || null xs
+  if shouldShowReductionStep configuration stepDescription || null xs
     then do
       if amountOfSkippedSteps > 0
         then putStrLn ("{- skipping " ++ show amountOfSkippedSteps ++ " substeps -}")
         else putStr ""
       putStrLn ("{- " ++ show stepDescription ++ " -}")
-      prettyPrint printingStyle expression
-      printStepResultList 0 printingStyle xs successFlat
+      prettyPrint (printingStyle configuration) expression
+      printStepResultList 0 configuration xs successFlat
     else
-      printStepResultList (amountOfSkippedSteps + 1) printingStyle xs successFlat
+      printStepResultList (amountOfSkippedSteps + 1) configuration xs successFlat
 
 getAllSteps :: [Binding] -> CoreExpr -> ([StepResult], ReductionSuccessfulFlag)
 getAllSteps bindings expression = do
@@ -105,15 +103,14 @@ convertToBindingsList = concatMap convertCoreBindingToBindingList
     convertCoreBindingToBindingList (Rec bindings) = bindings
 
 
-shouldFilterOutReductionStep :: PrintingStyle -> ReductionStepDescription -> Bool
-shouldFilterOutReductionStep HaskellStyle (ApplicationStep argument) = isTypeInformation argument --do not show application of type variables, those informations are not shown when pretty printing haskell-like code
-shouldFilterOutReductionStep HaskellStyle (EvaluationStep function) = isPrimitiveTypeConstructorName (varName function)
-shouldFilterOutReductionStep style (NestedReduction nestedReduction) = shouldFilterOutReductionStep style (last nestedReduction)
-shouldFilterOutReductionStep _ _ = False
+shouldFilterOutReductionStepForPrintingStyle :: PrintingStyle -> ReductionStepDescription -> Bool
+shouldFilterOutReductionStepForPrintingStyle HaskellStyle (ApplicationStep argument) = isTypeInformation argument --do not show application of type variables, those informations are not shown when pretty printing haskell-like code
+shouldFilterOutReductionStepForPrintingStyle HaskellStyle (EvaluationStep function) = isPrimitiveTypeConstructorName (varName function)
+shouldFilterOutReductionStepForPrintingStyle style (NestedReduction nestedReduction) = shouldFilterOutReductionStepForPrintingStyle style (last nestedReduction)
+shouldFilterOutReductionStepForPrintingStyle _ _ = False
 
-shouldShowReductionStep :: ReductionStepDescription -> Bool
-shouldShowReductionStep (EvaluationStep _) = True
-shouldShowReductionStep PatternMatchStep = True
-shouldShowReductionStep ConstructorArgumentReductionForVisualization = True
-shouldShowReductionStep (NestedReduction reductions) = False
-shouldShowReductionStep _ = False
+shouldShowReductionStep :: StepperOutputConfiguration -> ReductionStepDescription -> Bool
+shouldShowReductionStep _ (EvaluationStep _) = True
+shouldShowReductionStep _ PatternMatchStep = True
+shouldShowReductionStep _ ConstructorArgumentReductionForVisualization = True
+shouldShowReductionStep configuration _ = False
